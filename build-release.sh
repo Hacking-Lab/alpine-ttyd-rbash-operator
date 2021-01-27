171bf889afa7b6601226a9e337687d20ce1f19b7
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-27 11:44:10 +0100

    update

---
+++ build-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf ./docker-alpine-libwebsockets-with-libuv
+git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
+
+rm -rf ./app
+mv ./docker-alpine-libwebsockets-with-libuv/app .
+
+docker build --no-cache -t hackinglab/alpine-ttyd-rbash-operator:$1.0 -t hackinglab/alpine-ttyd-rbash-operator:$1 -t hackinglab/alpine-ttyd-rbash-operator:latest -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-ttyd-rbash:$1.0 -t hackinglab/alpine-ttyd-rbash:$1 -t hackinglab/alpine-ttyd-rbash:latest -f Dockerfile .
+
+docker push hackinglab/alpine-ttyd-rbash-operator
+docker push hackinglab/alpine-ttyd-rbash-operator:$1
+docker push hackinglab/alpine-ttyd-rbash-operator:$1.0
+docker push hackinglab/alpine-ttyd-rbash
+docker push hackinglab/alpine-ttyd-rbash:$1
+docker push hackinglab/alpine-ttyd-rbash:$1.0
+
+
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -rf ./docker-alpine-libwebsockets-with-libuv
 git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
 
@@ -5,13 +6,8 @@ rm -rf ./app
 mv ./docker-alpine-libwebsockets-with-libuv/app .
 
 docker build --no-cache -t hackinglab/alpine-ttyd-rbash-operator:3.2.0 -t hackinglab/alpine-ttyd-rbash-operator:3.2 -t hackinglab/alpine-ttyd-rbash-operator:latest -f Dockerfile .
+
 docker build --no-cache -t hackinglab/alpine-ttyd-rbash:3.2.0 -t hackinglab/alpine-ttyd-rbash:3.2 -t hackinglab/alpine-ttyd-rbash:latest -f Dockerfile .
 
-docker push hackinglab/alpine-ttyd-rbash-operator
-docker push hackinglab/alpine-ttyd-rbash-operator:3.2
-docker push hackinglab/alpine-ttyd-rbash-operator:3.2.0
-docker push hackinglab/alpine-ttyd-rbash
-docker push hackinglab/alpine-ttyd-rbash:3.2
-docker push hackinglab/alpine-ttyd-rbash:3.2.0
-
+rm -rf ./docker-alpine-libwebsockets-with-libuv
 
